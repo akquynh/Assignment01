8df386467f0790b935a5804d434085261f9904c6
akquynh <akquynh3@gmail.com>
2024-09-24 14:32:04 -0700

    added code

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// Convert identifiers to Camel Case notation
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+// Example of Numeric Literal Expression
+let days = 7;
+
+// Example of Literal Expression
+let birthday = "Today is your birthday!";
+
+// Example of Boolean Expresion 
+let today = true; 
+
+// Example of Null Literal Expression 
+let month = null; 
+
+// Example of Variable Expression - Area of a Triangle
+let b = 25;
+let h = 3;
+let (b * h)/2;
+
+// Example of Complex Expression - Calculate Sales Tax & Final Price
+function calculateTax(price) {
+    return (price * 0.0775)+ price; 
+}
+let totalPrice = 50.75;
+let discount = calculateTax(totalPrice); 
+
+// Declare & Assign Variables
+let strFirstName;
+let strLastName;
+let strAddress;
+let strCity = "San Diego";
+let strState = "California";
+let numZipCode = 92037;
+let numYourAge;
+let strReferralSource;
+let boolMayWeContactYou;
+
+// Create a number and string variable with coerced result 
+let happyBirthday = "Happy Birthday! You are " + 18 + " years old today.";
+console.log(happyBirthday);
+
+// Create a boolean and string variable with coerced result 
+let day = true + "today is Monday."
+console.log(day);
+
+// Create a boolean and number variable with coerced result
+let thisMonth = 12 + false;
+console.log(thisMonth);
+
+// Fix code
+let someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+console.log(someString);
+
+// Create null variable 
+let nullVariable = null;
+console.log(nullVariable);
+
+// Create undefined variable 
+let undefVariable;
+console.log(undefVariable);
+
+// Create literal string 
+console.log(typeof "Good Afternoon");
+
+// Create literal number
+console.log(typeof 1998);  
+
+// Create literal boolean
+console.log(typeof true);  
+
+// Create literal object
+console.log(typeof null); 
+
+// Create literal undefined
+let someVariable;
+console.log(typeof someVariable);
+
+
+
+
